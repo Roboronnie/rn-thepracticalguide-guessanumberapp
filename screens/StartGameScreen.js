34e2f3c48ec90ebe89aac35a6b276dd2ecd0ffcc
Roboronnie <leex4920@gmail.com>
2020-04-07 21:41:09 -0500

    add usability to input with Keyboard api, touchablewithoutfeedback and value set

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,65 +1,89 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
-import Card from '../components/Card';
-import Input from '../components/Input';
+import Card from "../components/Card";
+import Input from "../components/Input";
 
-import Colors from '../constants/colors';
+import Colors from "../constants/colors";
 
-const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start A New Game</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select A Number</Text>
-                <Input 
-                    style={styles.input} 
-                    blurOnSubmit autoCapitalize='none' 
-                    autoCorrect={false} 
-                    keyboardType="number-pad" 
-                    maxLength={2}
-                />
-                <View style={styles.buttonContainer}>
-                    <View syle={styles.button}>
-                        <Button title="Reset" onPress={()=> {} } color={Colors.accent}/>
-                    </View>
-                    <View syle={styles.button}>
-                        <Button title="Confirm" onPress={()=> {}} color={Colors.primary}/>
-                    </View>
-                </View>
-            </Card>
-        </View>
-    );
+const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => { 
+        Keyboard.dismiss();
+    }}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start A New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select A Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View syle={styles.button}>
+              <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+            </View>
+            <View syle={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 10,
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 20,
-        marginVertical: 10,
-    }, 
-    inputContainer: {
-        width: 275,
-        maxWidth: '80%',
-        alignItems: 'center',
-    }, 
-    buttonContainer: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15
-    },
-    button: {
-        width: 100
-    },
-    input: {
-        width: 50,
-        textAlign: 'center'
-    }
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 275,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;
